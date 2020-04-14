547c0b52c7c596a42b33606d6168dbf256acbded
Jin Wu <jinn.wuu@gmail.com>
2020-04-14 09:45:35 -0400

    real-time price

--- src/containers/ProfileStock.jsx
+++ src/containers/ProfileStock.jsx
@@ -2,17 +2,33 @@ import React from 'react'
 
 class ProfileStock extends React.Component {
 
+    state = {
+        realTimeStock: []
+    }
+
+    componentDidMount() {
+        fetch(`https://financialmodelingprep.com/api/v3/stock/real-time-price/${this.props.stock.symbol}`)
+        .then(r=> r.json())
+        .then(results => {
+          this.setState({
+            realTimeStock: results
+          })
+        })
+    }
+
     handleDelete = () => {
         this.props.deleteStock(this.props.stock.id)
-        this.props.sellStocks(this.props.user.id, this.props.stock.price)
+        this.props.sellStocks(this.props.user.id, this.state.realTimeStock.price)
     }
 
     render() {
+        console.log(this.state.realTimeStock)
         let {symbol, name, price, exchange} = this.props.stock
         return (
             <div>
                 <h3>{symbol}: {name}</h3>
-                <h4>${price}</h4>
+                <h4>Cost: ${price}</h4>
+                <h4>Current Price: ${this.state.realTimeStock.price}</h4>
                 <h5>{exchange}</h5>
                 <button onClick={this.handleDelete}>remove</button>
             </div>
